1d14c0987a0b57bc2a880bd6ead189600dd08c91
穆华岭 <hualing.mu@outlook.com>
2019-12-26 09:18:26 +0800

    create Todo item component

---
+++ src/components/todo-item.tsx
@@ -0,0 +1,32 @@
+// Import dependencies and interfaces
+import * as React from "react"
+import {TodoItemInterface} from "./../interfaces"
+
+// TodoItem Component
+const TodoItem = (props: TodoItemInterface) => {
+    return (
+        <div className="todo-item">
+            <div className="item-remove" onClick={() => props.handleTodoRemove(props.todo.id)}>
+                ×
+            </div>
+            
+            <div onClick={() => props.handleTodoComplete(props.todo.id)}>
+                {props.todo.isCompleted ? (
+                    <span className="todo-item-checked">√</span>
+                ) : (
+                    <span className="todo-item-unchecked"></span>
+                )}
+            </div>
+
+            <div className="todo-item-input-wrapper">
+                <input
+                    value={props.todo.text}
+                    onBlur={props.handleTodoBlur}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => props.handleTodoUpdate(event, props.todo.id)}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default TodoItem
